fafca66327cff480f49ae479d18c3f5eef3d3446
Perrine Launay <plaunay@medinplus.com>
2026-07-11 15:05:02 +0200

    ML-54 admin user list

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -13,7 +13,8 @@ import MessagingPage from './pages/MessagingPage';
 import AgendaPage from './pages/AgendaPage';
 import ExportPage from './pages/ExportPage';
 import AccountPage from './pages/AccountPage';
-import { ROLE_AIDANT, ROLE_PATIENT, ROLE_SOIGNANT } from './services/roles';
+import AdminUsersPage from './pages/AdminUsersPage';
+import { ROLE_ADMIN, ROLE_AIDANT, ROLE_PATIENT, ROLE_SOIGNANT } from './services/roles';
 
 function App() {
   return (
@@ -41,6 +42,9 @@ function App() {
         </Route>
         <Route element={<ProtectedRoute roles={[ROLE_AIDANT, ROLE_SOIGNANT]} />}>
           <Route path="/invitations" element={<InvitationsPage />} />
+        </Route>
+        <Route element={<ProtectedRoute roles={[ROLE_ADMIN]} />}>
+          <Route path="/admin/users" element={<AdminUsersPage />} />
         </Route>
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
